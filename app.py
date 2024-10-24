bf874eab6a0275214ead4800e88cae981b8c2ce4
Vonzinho <gugasheidt36@gmail.com>
2024-10-24 14:38:33 -0300

    ajustes na tela de registro

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask (__name__)
 def home (): 
     return render_template('index.html')
 
+@app.route ('/registro')
+def registro (): 
+    return render_template('registro.html')
 
 if __name__ == '__main__': 
     app.run (debug=True)
